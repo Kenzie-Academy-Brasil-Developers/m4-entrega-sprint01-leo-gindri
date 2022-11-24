347e73b361d237372b960a81f7f58fdf587ba8f6
Leonardo <leo.gindri@outlook.com>
2022-11-23 23:32:39 -0300

    finalizando atividade

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import express from "express";
 
+const app = express();
 
-const app = express()
+app.get("/", (req, resp) => {
+  return resp.send(
+    "Saudade do next, acho que vou meter o louco e fazer algum site ;D"
+  );
+});
 
+const PORT = 3000;
 
-export default app
+app.listen(PORT, () => {
+  return "App funcioanando corretamente";
+});
+
+export default app;
